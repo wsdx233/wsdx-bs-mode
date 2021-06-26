611bbcba809bc4f6cd152335b2c6c7c97bf44ffa
wsdx233 <18082792459@163.com>
2021-06-27 07:08:26 +0800

    更新两个mod

---
+++ UnlockPro.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# 解锁器自动生成，请勿修改
+import re;
+import base64;
+from bsUtils import *
+import bsUtils;
+def _unlockedPro():
+        return True
+def _boughtPro():
+        return True
+bsUtils._haveProOptions = _unlockedPro;
+bsUtils._havePro = _boughtPro;
