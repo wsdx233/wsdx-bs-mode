9f5f0b6e699507b8a43a7291d285fd7c89ea7417
wsdx233 <18082792459@163.com>
2021-07-16 09:35:07 +0800

    新增了一个SH安装器用于安装mod管理器

---
+++ bsmodins.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import time
+
+bpath = input('输入炸队模组文件夹路径:')
+baselink = 'https://gitee.com/wsdx233/wsdx-bs-mode/raw/master/'
+
+mods = json.loads(
+  requests.get(baselink+'mods.json').text
+  )['mods']
+
+while True:
+  for ind in range(len(mods)) :
+    print("\n")
+    mod = mods[ind]
+    print("["+str(ind)+"]"+mod['name']+" "*2+"by "+mod['author'])
+    print(mod['info'])
+  
+  num = input('安装序号(exit退出):')
+  
+  if num == 'exit':
+    break
+  
+  dmod = mods[int(num)]
+  
+  for fname in dmod['files'] :
+    if not bpath.endswith('/') :
+      bpath = bpath + '/'
+    with open(bpath+fname,'w') as f:
+      f.write(requests.get(baselink+fname).text)
+      print(bpath+fname+"已安装")
+  
+  print('\n模组 '+dmod['name']+' 安装完成!五秒后返回模组列表！')
+  time.sleep(5)
+  
---
+++ managers/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+python -V
+if [ $? -eq 0 ]; then
+  echo 您已安装Python，将为您下载模组管理器
+else
+  echo 您未安装Python，正在为您安装Python
+  apt-get update
+  apt-get install python
+fi
+
+echo 正在为您安装必要的python库
+pip install requests
+
+echo 正在Mod管理器
+wget https://gitee.com/wsdx233/wsdx-bs-mode/raw/master/managers/bsmodins.py
+
+echo 启动Mod管理器
+python managers/bsmodins.py
